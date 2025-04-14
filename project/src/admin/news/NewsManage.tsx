96a391cdc2643522346c6c403c87a0ec7abe655e
soysomon <mysrox07@gmail.com>
2025-04-14 13:23:36 -0400

    Agregar Tipo de noticia cultural a crear noticias

--- project/src/admin/news/NewsManage.tsx
+++ project/src/admin/news/NewsManage.tsx
@@ -115,6 +115,8 @@ const NewsManage: React.FC<{ onEdit: (id: string) => void }> = ({ onEdit }) => {
             <option value="Todas">Todas las categorías</option>
             <option value="General">General</option>
             <option value="Académico">Académico</option>
+            <option value="Académico">Cultural</option>
+            
           </select>
         </div>
       </div>
