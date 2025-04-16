9526ceffbc401cf4e2109dc25a502c137685ea79
soysomon <mysrox07@gmail.com>
2025-04-16 09:05:27 -0400

    He actualizado el CSS and el MainLayout

--- project/src/components/MainLayout.tsx
+++ project/src/components/MainLayout.tsx
@@ -4,16 +4,18 @@ import { Outlet } from 'react-router-dom';
 import Navigation from './Navigation';
 import { QuickNav } from './QuickNav';
 import { Footer } from './Footer';
+import ChatBot from './ChatBot'; // Importar ChatBot
 
 const MainLayout: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navigation />
       <QuickNav />
-      <main>
+      <main className="min-h-[calc(100vh-200px)]"> {/* Ajustar altura mínima */}
         <Outlet /> {/* Render the child routes here */}
       </main>
       <Footer />
+      <ChatBot /> {/* Añadir ChatBot */}
     </div>
   );
 };
