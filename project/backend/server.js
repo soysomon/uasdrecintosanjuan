5e2fca10004779db517ab499f4a265bd3cb60189
ED127325 <elianny_delossantosdelossantos@bhd.com.do>
2025-07-28 05:41:19 -0400

    Agrega configuración de dominio www  en server.js

--- project/backend/server.js
+++ project/backend/server.js
@@ -22,6 +22,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://uasdrecintosanjuan-fe-production.up.railway.app',
   'https://uasdrecintosanjuan.org',
+  //New
   'https://www.uasdrecintosanjuan.org'
 ];
 
